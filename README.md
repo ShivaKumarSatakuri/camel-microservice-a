dbf3fbc0b03247e3f7d15bcf3e3ba7602df4e5b6
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-06-02 17:29:51 +0530

    Created basic structure for camel microservice a

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # camel-microservice-a
+
+<i> This is a spring boot microservice(a) using apache camel</i>
---
+++ src/main/java/com/camelmicroservicea/CamelMicroserviceAApplication.java
@@ -0,0 +1,11 @@
+package com.camelmicroservicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceAApplication {
+    public static void main(String... args) {
+        SpringApplication.run(CamelMicroserviceAApplication.class);
+    }
+}

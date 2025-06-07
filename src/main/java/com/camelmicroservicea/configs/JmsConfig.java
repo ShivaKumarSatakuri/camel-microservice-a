5ded77ee2375714711dd50e72e7820393de41ee7
ShivaKumarSatakuri <shivakumarsatakuri@gmail.com>
2025-06-07 23:27:52 +0530

    JmsConfig - Added JmsConfig class to check working of messages

---
+++ src/main/java/com/camelmicroservicea/configs/JmsConfig.java
@@ -0,0 +1,22 @@
+package com.camelmicroservicea.configs;
+
+import jakarta.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(
+            @Value("${spring.activemq.broker-url}") String brokerUrl,
+            @Value("${spring.activemq.user}") String user,
+            @Value("${spring.activemq.password}") String password) {
+
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(user, password, brokerUrl);
+        factory.setTrustAllPackages(true);
+        return factory;
+    }
+}
